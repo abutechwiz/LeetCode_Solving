73ae21d0be1225394bb6a55799118a725ba54910
abutechwiz <abdur12shariff@gmail.com>
2021-11-18 21:33:57 +0530

    added 53

---
+++ 53_maximum_subarray.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    int maxSubArray(vector<int>& nums) {
+        int max_sum=INT_MIN,curr_sum=0;
+        for(int i=0;i<nums.size();i++){
+            curr_sum+=nums[i];
+            if(curr_sum>max_sum)
+                max_sum=curr_sum;
+            if(curr_sum<0)
+                curr_sum=0;
+        }
+        return max_sum;
+    }
+
+    int main(){
+    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+    cout << maxSubArray(nums) << "\n";
+    //cout << ui << "\n";
+
+}
